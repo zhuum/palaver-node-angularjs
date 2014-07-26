7c27f36771b350e56f131ffe5bda605129fc9e63
Marcus <marcus.mckinnon@gmail.com>
2014-07-26 11:14:15 -0500

    removed old test files

--- massive.js
+++
@@ -1,9 +0,0 @@
-var db = require('massive');
-db.connect('postgres://postgres:password@localhost/palaver', function(err,db) {
-	// db.comments.each(function(x){
-	// 	console.log(x);
-	// });
-	db.disconnect();
-});
-
-console.log('done.');
